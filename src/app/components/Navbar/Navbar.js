8c5f51de92ce629ec09530b1d12042fb4c98ed93
akmindaugas <mindaugas.akelis@gmail.com>
2024-06-06 19:08:28 +0300

    commit Navbar not done yet

---
+++ src/app/components/Navbar/Navbar.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import logo from './images/logo.png'
+import './Navbar.css'
+use client; 
+function Navbar() {
+    const [click, setClick] = useState(false);
+  
+    const handleClick = () => setClick(!click);
+  
+    const closeMenu = () => setClick(false);
+  
+    return (
+      <div className={styles.container}>
+        <div className='header'>
+          <nav className='navbar'>
+            <a href='/' className='logo'>
+              <img src={logo} alt='logo' />
+            </a>
+            <div className='hamburger' onClick={handleClick}>
+              {click ? (
+                <FaTimes size={30} style={{ color: '#ffffff' }} />
+              ) : (
+                <FaBars size={30} style={{ color: '#ffffff' }} />
+              )}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className='nav-item'>
+                <a href='/' onClick={closeMenu}>Home</a>
+              </li>
+              {/* ... other menu items */}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    );
+  }
+  
+  export default Navbar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import styles from "./page.module.css";
+import Navbar from "./components/Navbar/Navbar"
 
 export default function Home() {
   return (
     <main className={styles.main}>
+       <Navbar />
       <h1>
         home
       </h1>
