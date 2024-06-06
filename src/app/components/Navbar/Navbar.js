eecbc1d6e1dd981e1cb0000bb9e567ee8203af18
akmindaugas <mindaugas.akelis@gmail.com>
2024-06-06 22:01:27 +0300

    commit some dev

--- src/app/components/Navbar/Navbar.js
+++ src/app/components/Navbar/Navbar.js
@@ -13,27 +13,33 @@ function Navbar() {
   
     return (
       <div className={styles.container}>
-        <div className='header'>
-          <nav className='navbar'>
-            <a href='/' className='logo'>
+        {/* <div className='header'> */}
+          <nav className={styles.navbar}>
+            <a href='/' className={styles.logo}>
               <img src={logo} alt='logo' />
             </a>
-            <div className='hamburger' onClick={handleClick}>
+            <div className={styles.hamburger} onClick={handleClick}>
               {click ? (
                 <FaTimes size={30} style={{ color: '#ffffff' }} />
               ) : (
                 <FaBars size={30} style={{ color: '#ffffff' }} />
               )}
             </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <ul className={click ? `${styles.navMenu} active` : styles.navMenu}>
               <li className='nav-item'>
                 <a href='/' onClick={closeMenu}>Home</a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' onClick={closeMenu}>About</a>
+              </li>
+              <li className='nav-item'>
+                <a href='/' onClick={closeMenu}>Gallery</a>
               </li>
               {/* ... other menu items */}
             </ul>
           </nav>
         </div>
-      </div>
+      // </div>
     );
   }
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import React, {useState, useEffect} from "react";
 import styles from "./page.module.css";
 import Navbar from "./components/Navbar/Navbar"
+import About from "./components/About/About"
 
 export default function Home() {
   return (
     <main className={styles.main}>
        <Navbar />
+       <About/>
       <h1>
         home
       </h1>
